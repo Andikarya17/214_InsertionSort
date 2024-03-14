b7d1c1ce4b4d6dbb5265a80e9c02ffe2c0ac53ed
Andikarya17 <andikaryaputra17@gmail.com>
2024-03-14 14:53:53 +0700

    commit 2

--- 214_InsertionSort/214_InsertionSort.cpp
+++ 214_InsertionSort/214_InsertionSort.cpp
@@ -6,7 +6,17 @@ void InsertionSort() {
     int arr[20];
 
     for (i = 1; i <= n - 1; i++) { //step 1 
+        temp = arr[i]; //step2
 
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--; //step 4
+        }
+
+        arr[j + 1] = temp; //step 5
     }
 }
 
