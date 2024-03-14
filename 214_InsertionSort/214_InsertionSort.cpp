051bf5cf02c3964c2c87b99fdf92bf53af6c3874
Andikarya17 <andikaryaputra17@gmail.com>
2024-03-14 14:40:51 +0700

    Add project files.

---
+++ 214_InsertionSort/214_InsertionSort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void InsertionSort() {
+    int i, j, n, temp;
+    int arr[20];
+
+    for (i = 1; i <= n - 1; i++) { //step 1
+
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
